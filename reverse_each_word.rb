cbb503ef337811e1b2a84f1150928a7e6ae4dce8
Jasper Reed <jmixon.reed@gmail.com>
2020-07-20 21:32:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(sentence)
-  rev_sent = nil
   array_sentence = sentence.split(" ")
   array_sentence.collect do |rev|
-    rev.reverse()
+    rev.reverse().join()
   end
-  array_sentence
 end
