a239bdd3efc7363661173c392fd4c41c878c84e8
Jasper Reed <jmixon.reed@gmail.com>
2020-07-20 21:31:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   rev_sent = nil
   array_sentence = sentence.split(" ")
   array_sentence.collect do |rev|
-    rev_sent = rev.reverse()
+    rev.reverse()
   end
-  rev_sent
+  array_sentence
 end
